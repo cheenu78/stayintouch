c4d96dbec27dd915014825588418c0e8bc880e2b
Srinivasa Gopalan Parthasaathi <cheenugit@gmail.com>
2012-07-14 14:51:34 +1000

    sts commit

--- StayInTouch/src/main/java/org/srini/stayintouch/advice/LogAdvice.java
+++ StayInTouch/src/main/java/org/srini/stayintouch/advice/LogAdvice.java
@@ -35,5 +35,6 @@ public class LogAdvice {
 	@Before("getDays(year,month)")
 	public void beforeGetDays(Integer year, String month, JoinPoint joinPoint) {
 		logger.info("Year =>"+year+" month =>"+month);
+		//sts is adding this line
 	}
 }
